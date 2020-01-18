0861a245c9aa8d5cc9def7a2612d0a88e427e184
Danny Ruchtie <46239581+DannyRuchtie@users.noreply.github.com>
2020-01-18 17:33:19 +0100

    Added framer motion
    Added framer motion  need to connect it to a keyboard shortcut

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import "./styles.css";
 import ListItem from "./ListItem.js";
 import Canvas from "./Canvas.js";
 
 function App() {
+  const [active, setActive] = React.useState(false);
+
   return (
     <div className="container fullscreen" id="fullsccreen">
-      <div className="navbar">
-        <div className="header font-title">Moodoard</div>
-        <ul>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-        </ul>
-        <div className="footer ont-secondary" />
-      </div>
+      <motion.div
+        className="rectangle"
+        animate={active ? { opacity: 1, x: "-100%" } : { opacity: 1, x: 0 }}
+        onClick={() => setActive(!active)}
+      >
+        <div className="navbar">
+          <div className="header font-title">Moodoard</div>
+          <ul>
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+          </ul>
+          <div className="footer ont-secondary" />
+        </div>
+      </motion.div>
 
       <Canvas />
     </div>
